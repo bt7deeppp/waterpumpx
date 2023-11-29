b1bc9286669705adc681dee9f7d1476ea1d7b555
bt7deeppp <151866452+bt7deeppp@users.noreply.github.com>
2023-11-29 20:48:24 +0530

    Update waterpumpx.ino

--- waterpumpx.ino
+++ waterpumpx.ino
@@ -1,20 +1,36 @@
+#define BLYNK_TEMPLATE_ID "TMPL3yrqGwx2M"
+#define BLYNK_TEMPLATE_NAME "Switch On"
+#define BLYNK_AUTH_TOKEN "kWKOXxQeO0EX0g_7pDqJ3EOWsFjPd4qx"
+
 #define BLYNK_PRINT Serial
-#define BLYNK_TEMPLATE_ID "TMPL3mzzLThPK"
-#define BLYNK_TEMPLATE_NAME "waterpumpx"
-#define BLYNK_AUTH_TOKEN "ueROYOd_mrGeNezipwvKL6rdRvfih72e"
 
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 
+BLYNK_WRITE(V0)
+{
+  int value = param.asInt();
+  Serial.println(value);
+  if(value == 1)
+  {
+    Serial.println("on");
+    digitalWrite(D0,LOW);
+  }
+  if(value == 0)
+  {
+    Serial.println("off");
+    digitalWrite(D0,HIGH);
+  }
+}
+
 void setup()
 {
-  Serial.begin(9600);
-  Blynk.begin(BLYNK_AUTH_TOKEN, "swarnadeep", "class7bc");
+  Serial.begin(115200);
+  Blynk.begin(BLYNK_AUTH_TOKEN, "Pump", "op&*deep");
+  pinMode(D0,OUTPUT);
 }
 
 void loop()
 {
   Blynk.run();
 }
-
-
